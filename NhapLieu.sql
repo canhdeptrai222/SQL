f90b3ed74c26c2e096ad3d03ae629865cc4bcef1
canhdeptrai222 <158004919+canhdeptrai222@users.noreply.github.com>
2024-06-28 10:49:07 +0700

    Add files via upload

---
+++ NhapLieu.sql
@@ -0,0 +1,16 @@
+--Nhap DL cho bang KhachHang
+select * from QuanLyBanHang.dbo.Khachhang
+insert into QuanLyBanHang.dbo.Khachhang
+select * from Tam.dbo.Khachhang$
+--Bang vattu
+select * from QuanLyBanHang.dbo.VatTu
+insert into QuanLyBanHang.dbo.VatTu
+select * from Tam.dbo.VatTu$
+--Bang Hoadon
+select * from QuanLyBanHang.dbo.HoaDon
+insert into QuanLyBanHang.dbo.HoaDon
+select * from Tam.dbo.HoaDon$
+--Bang CTHD
+select * from QuanLyBanHang.dbo.CTHD
+insert into QuanLyBanHang.dbo.CTHD
+select * from Tam.dbo.CTHD$
